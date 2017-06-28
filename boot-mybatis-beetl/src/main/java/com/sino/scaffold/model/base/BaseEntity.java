55901fd0c6a135a8c2151e67714109e56790ad47
Kerbores <Kerbores@gmail.com>
2017-06-28 12:40:29 +0800

    del:删除无用的字段

--- boot-mybatis-beetl/src/main/java/com/sino/scaffold/model/base/BaseEntity.java
+++ boot-mybatis-beetl/src/main/java/com/sino/scaffold/model/base/BaseEntity.java
@@ -1,7 +1,5 @@
 package com.sino.scaffold.model.base;
 
-import javax.persistence.Transient;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -11,28 +9,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  *
  */
 public class BaseEntity {
-
-	@Transient
-	private transient Integer page = 1;
-
-	@Transient
-	private transient Integer rows = 10;
-
-	public Integer getPage() {
-		return page;
-	}
-
-	public void setPage(Integer page) {
-		this.page = page;
-	}
-
-	public Integer getRows() {
-		return rows;
-	}
-
-	public void setRows(Integer rows) {
-		this.rows = rows;
-	}
 
 	@Override
 	public String toString() {
