1a59f5055d3a6c1a7b1ca054ee1cbc84f304444d
Kerbores <Kerbores@gmail.com>
2017-06-28 15:48:02 +0800

    add:nutz版本

---
+++ boot-nutz-beetl/src/main/java/com/sino/scaffold/BootNutzBeetlApplication.java
@@ -0,0 +1,12 @@
+package com.sino.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootNutzBeetlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootNutzBeetlApplication.class, args);
+	}
+}
