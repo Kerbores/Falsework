4fe0344799a446adae2dfa4a5b5dbb0bd7da8311
Kerbores <Kerbores@gmail.com>
2017-06-29 13:35:08 +0800

    add:mybatis-plus版

---
+++ boot-mybatis-plus-beetl/src/main/java/com/sino/scaffold/BootMybatisPlusBeetlApplication.java
@@ -0,0 +1,12 @@
+package com.sino.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMybatisPlusBeetlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMybatisPlusBeetlApplication.class, args);
+	}
+}
