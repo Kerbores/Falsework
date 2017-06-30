a4a3bf54f76fbd49639d4b0f3483057e54c33cd7
Kerbores <Kerbores@gmail.com>
2017-06-30 14:23:44 +0800

    add:鉴权示例及统一异常处理

--- boot-nutz-beetl/src/main/java/com/sino/scaffold/controller/acl/UserController.java
+++ boot-nutz-beetl/src/main/java/com/sino/scaffold/controller/acl/UserController.java
@@ -1,5 +1,6 @@
 package com.sino.scaffold.controller.acl;
 
+import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sino.scaffold.Result;
+import com.sino.scaffold.bean.InstalledRole;
+import com.sino.scaffold.ext.shiro.anno.SINORequiresRoles;
 import com.sino.scaffold.service.acl.ShiroUserService;
 
 /**
@@ -23,6 +26,18 @@ public class UserController {
 	@GetMapping("login")
 	public Result login(@RequestParam String userName, @RequestParam String password) {
 		return shiroUserService.login(userName, password);
+	}
+
+	@GetMapping("logout")
+	public Result logout() {
+		SecurityUtils.getSubject().logout();
+		return Result.success();
+	}
+
+	@GetMapping("shiro")
+	@SINORequiresRoles(InstalledRole.SU)
+	public Result shiro() {
+		return Result.success();
 	}
 
 }
---
+++ boot-nutz-beetl/src/main/java/com/sino/scaffold/hanlder/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.sino.scaffold.hanlder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.apache.shiro.authz.UnauthenticatedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sino.scaffold.OperationState;
+import com.sino.scaffold.Result;
+
+@RestController
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	Logger logger = Logger.getLogger(getClass());
+
+	@ExceptionHandler(value = Exception.class)
+	public Result defaultErrorHandler(HttpServletRequest req, HttpServletResponse response, Exception e)
+			throws Exception {
+		logger.error("error=>", e);
+		if (e instanceof UnauthenticatedException) {
+			response.setStatus(401);
+			return Result.me().setOperationState(OperationState.UNLOGINED).addData("msg", "没有权限");
+		}
+		return Result.exception(e);
+	}
+}
