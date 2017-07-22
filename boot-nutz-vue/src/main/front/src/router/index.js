6e2c36ebbb3c6eb9d67e9e638838a20527ac3b90
Kerbores <Kerbores@gmail.com>
2017-07-22 15:10:07 +0800

    delete: 删除无用的代码

--- boot-nutz-vue/src/main/front/src/router/index.js
+++ boot-nutz-vue/src/main/front/src/router/index.js
@@ -64,48 +64,8 @@ export default new Router({
                     name: 'oaoe',
                     path: '/organization/aoe/:id',
                     component: resolve => require(['../components/page/BranchAddOrEdit.vue'], resolve)
-                },
-                {
-                    path: '/map',
-                    component: resolve => require(['../components/page/Map.vue'], resolve)
-                },
-                {
-                    path: '/basetable',
-                    component: resolve => require(['../components/page/BaseTable.vue'], resolve)
-                },
-                {
-                    path: '/vuetable',
-                    component: resolve => require(['../components/page/VueTable.vue'], resolve) // vue-datasource组件
-                },
-                {
-                    path: '/baseform',
-                    component: resolve => require(['../components/page/BaseForm.vue'], resolve)
-                },
-                {
-                    path: '/vueeditor',
-                    component: resolve => require(['../components/page/VueEditor.vue'], resolve) // Vue-Quill-Editor组件
-                },
-                {
-                    path: '/markdown',
-                    component: resolve => require(['../components/page/Markdown.vue'], resolve) // Vue-Quill-Editor组件
-                },
-                {
-                    path: '/upload',
-                    component: resolve => require(['../components/page/Upload.vue'], resolve) // Vue-Core-Image-Upload组件
-                },
-                {
-                    path: '/basecharts',
-                    component: resolve => require(['../components/page/BaseCharts.vue'], resolve) // vue-echarts-v3组件
-                },
-                {
-                    path: '/mixcharts',
-                    component: resolve => require(['../components/page/MixCharts.vue'], resolve) // vue-echarts-v3组件
                 }
             ]
-        },
-        {
-            path: '/mobile',
-            component: resolve => require(['../components/page/Mobile.vue'], resolve)
         },
         {
             path: '/login',
