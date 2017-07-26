10ac213eb0efd73ef2121173d5aa6f56ee22e003
Kerbores <Kerbores@gmail.com>
2017-07-26 11:08:40 +0800

    add: 使用 axe 中的 swagger 模块

--- boot-nutz-vue/src/main/java/com/sino/scaffold/BootNutzVueApplication.java
+++ boot-nutz-vue/src/main/java/com/sino/scaffold/BootNutzVueApplication.java
@@ -29,7 +29,6 @@ import org.springframework.core.MethodParameter;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -53,17 +52,8 @@ import com.sino.scaffold.service.acl.UserRoleService;
 import com.sino.scaffold.service.acl.UserService;
 import com.sino.scaffold.service.qa.NutzerService;
 
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
 @EnableRedisHttpSession
-@EnableSwagger2
 @EnableAsync
 public class BootNutzVueApplication extends WebMvcConfigurerAdapter {
 
@@ -169,30 +159,6 @@ public class BootNutzVueApplication extends WebMvcConfigurerAdapter {
 
 		});
 		application.run(args);
-	}
-
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.genericModelSubstitutes(DeferredResult.class)
-				.useDefaultResponseMessages(false)
-				.forCodeGeneration(true)
-				.pathMapping("/")
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.sino"))
-				.build()
-				.apiInfo(apiInfo());
-	}
-
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("F")
-				.description("接口手册")// 详细描述
-				.version("1.0")// 版本
-				.termsOfServiceUrl("http://www.sinosoft.com.cn")
-				.contact(new Contact("王贵源", "http://www.sinosoft.com.cn", "wangguiyuan@sinosoft.com.cn"))// 作者
-				.license("The Apache License, Version 2.0")
-				.licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-				.build();
 	}
 
 	public static class QAUserCheckInterceptor implements HandlerInterceptor {
